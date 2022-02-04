661c660aa13954f4802bf3a2e9d14b72a1e2d6f0
vvickydev <pham.vvic@gmail.com>
2022-02-04 13:53:02 +1100

    completing all tasks

--- js/index.js
+++ js/index.js
@@ -19,38 +19,49 @@
       5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
 *************************** */
 
-window.addEventListener('DOMContentLoaded', () => {
-  const start = document.querySelector('#start');
-  start.addEventListener('click', function (e) {
-    document.querySelector('#quizBlock').style.display = 'block';
-    start.style.display = 'none';
+window.addEventListener("DOMContentLoaded", () => {
+  const start = document.querySelector("#start");
+  start.addEventListener("click", function (e) {
+    document.querySelector("#quizBlock").style.display = "block";
+    start.style.display = "none";
   });
   // quizArray QUESTIONS & ANSWERS
   // q = QUESTION, o = OPTIONS, a = CORRECT ANSWER
   // Basic ideas from https://code-boxx.com/simple-javascript-quiz/
   const quizArray = [
     {
-      q: 'Which is the third planet from the sun?',
-      o: ['Saturn', 'Earth', 'Pluto', 'Mars'],
+      q: "Which is the third planet from the sun?",
+      o: ["Saturn", "Earth", "Pluto", "Mars"],
       a: 1, // array index 1 - so Earth is the correct answer here
     },
     {
-      q: 'Which is the largest ocean on Earth?',
-      o: ['Atlantic Ocean', 'Indian Ocean', 'Arctic Ocean', 'Pacific Ocean'],
+      q: "Which is the largest ocean on Earth?",
+      o: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
       a: 3,
     },
     {
-      q: 'What is the capital of Australia',
-      o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
+      q: "What is the capital of Australia",
+      o: ["Sydney", "Canberra", "Melbourne", "Perth"],
       a: 1,
+    },
+    {
+      q: "What geometric shape is generally used for stop signs??",
+      o: ["Square", "Hexagon", "Octagon", "Decagon"],
+      a: 2, // Octagon
+    },
+    {
+      q: "What is the rarest M&M colour?",
+      o: ["brown", "red", "green", "yellow"],
+      a: 0, // brown
     },
   ];
 
   // function to Display the quiz questions and answers from the object
   const displayQuiz = () => {
-    const quizWrap = document.querySelector('#quizWrap');
-    let quizDisplay = '';
+    const quizWrap = document.querySelector("#quizWrap");
+    let quizDisplay = "";
     quizArray.map((quizItem, index) => {
+      // id=radio_0_0 means radio question 0 option 0, radio_0_1 -> question 0, option 1
       quizDisplay += `<ul class="list-group">
                    Q - ${quizItem.q}
                     <li class="list-group-item mt-2" id="li_${index}_0"><input type="radio" name="radio${index}" id="radio_${index}_0"> ${quizItem.o[0]}</li>
@@ -66,24 +77,80 @@ window.addEventListener('DOMContentLoaded', () => {
   // Calculate the score
   const calculateScore = () => {
     let score = 0;
+    // quizItem -> is the object
+    // index is the index of the question
     quizArray.map((quizItem, index) => {
       for (let i = 0; i < 4; i++) {
         //highlight the li if it is the correct answer
         let li = `li_${index}_${i}`;
+        // id in HTML of the radio option
         let r = `radio_${index}_${i}`;
-        liElement = document.querySelector('#' + li);
-        radioElement = document.querySelector('#' + r);
+        liElement = document.querySelector("#" + li);
+        radioElement = document.querySelector("#" + r);
+        let ans = `radio_${index}_${quizItem.a}`;
 
         if (quizItem.a == i) {
-          //change background color of li element here
+          //change border color of li element here
+          document.getElementById(li).style.border = "solid green";
         }
 
         if (radioElement.checked) {
           // code for task 1 goes here
+          // if the radio element is checked, is it the correct answer?
+          if (r == ans) {
+            score++;
+          }
         }
       }
     });
+    displayScore(score);
   };
+
+  // adds score content to modal html
+  const displayScore = (score) => {
+    let modalBody = document.getElementById("modalBody");
+    console.log(modalBody);
+    let scoreContent = `You score is ${score}!`;
+    modalBody.innerHTML = scoreContent;
+  };
+
+  // reload the page
+  const btnReset = document.querySelector("#btnReset");
+  btnReset.addEventListener("click", () => {
+    window.location.reload();
+  });
+
+  // add event listener to the submit button
+  const btnSubmit = document.querySelector("#btnSubmit");
+  btnSubmit.addEventListener("click", () => {
+    calculateScore();
+  });
+
+  // add a timer for 60 seconds
+  let timeLeft = 61;
+  const timeDisplay = document.getElementById("time");
+
+  const timer = setInterval(displayTime, 1000);
+
+  function displayTime() {
+    timeLeft--;
+    // format the time to display as 01:00 ...
+    let timeString =
+      timeLeft < 10
+        ? `00:0${timeLeft}`
+        : timeLeft < 60
+        ? `00:${timeLeft}`
+        : "01:00";
+    // set HTML
+    timeDisplay.innerHTML = timeString;
+
+    // when timer reaches 0, stop the timer, calculate score and display the mdoal
+    if (timeLeft == 0) {
+      clearInterval(timer);
+      calculateScore();
+      $("#scoreModal").modal();
+    }
+  }
 
   // call the displayQuiz function
   displayQuiz();
